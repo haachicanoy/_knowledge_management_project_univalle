7be58a8f4e53b660e5a046e489256d28960e10f7
haachicanoy <harold22010@gmail.com>
2017-08-25 15:42:16 -0500

    Descriptive analysis

--- _scripts/loading_and_exploring_data.R
+++ _scripts/loading_and_exploring_data.R
@@ -1,12 +1,76 @@
-library(tidyverse)
-library(foreign)
+# Gestion del conocimiento - Proyecto Univalle: cargar y explorar datos
+# H. Achicanoy & C. Saavedra
+# 2017
 
+# R options
+options(warn = -1); options(scipen = 999)
+
+# Load packages
+suppressMessages(library(tidyverse))
+suppressMessages(library(modelr))
+suppressMessages(library(dplyr))
+suppressMessages(library(purrr))
+suppressMessages(library(broom))
+suppressMessages(library(tidyr))
+suppressMessages(library(ggplot2))
+suppressMessages(library(FactoMineR))
+suppressMessages(library(factoextra))
+suppressMessages(library(foreign))
+suppressMessages(library(haven))
+suppressMessages(library(corrplot))
+suppressMessages(library(polycor))
+suppressMessages(library(psych))
+suppressMessages(library(gplots))
+suppressMessages(library(viridis))
+
+# ------------------------------------------------------- #
+# Loading data
+# ------------------------------------------------------- #
 km_data <- read.spss(file = "../_data/Base GConocimiento PymeS  Valle_2017.sav", to.data.frame = T, use.value.labels = T) # F
 names(km_data)
 
-# Aprendizaje
-aprendizaje <- km_data %>% dplyr::select(P5_4, P5_5, P6_1, P6_2, P6_5, P7_2, P8_1, P13_13:P13_16)
+pilares <- km_data %>% select(P3_1:P3_8, P5_1:P9_6, P11_1:P13_16, P22_1:P25_5)
+for(j in 1:ncol(pilares)){
+  pilares[,j] <- factor(pilares[,j], levels = c("Totalmente en desacuerdo",
+                                                "En desacuerdo",
+                                                "Ni de acuerdo ni en desacuerdo",
+                                                "De acuerdo",
+                                                "Totalmente de acuerdo"), ordered = T)
+}; rm(j)
+pilares %>% glimpse
+pilares %>% str
+
+# ------------------------------------------------------- #
+# Pilar aprendizaje
+# ------------------------------------------------------- #
+aprendizaje <- pilares %>% dplyr::select(P5_4, P5_5, P6_1, P6_2, P6_5, P7_2, P8_1, P13_13:P13_16)
 aprendizaje %>% glimpse
+
+# Analisis descriptivo
+fqTable <- aprendizaje %>%
+  gather(measure, value) %>%
+  count(measure, value)
+names(fqTable) <- c("Variable", "Categoria", "Frecuencia")
+fqTable <- fqTable %>% dplyr::mutate(Porcentaje = Frecuencia/nrow(fqTable))
+fqTable$Categoria <- factor(fqTable$Categoria, levels = c("Totalmente en desacuerdo",
+                               "En desacuerdo",
+                               "Ni de acuerdo ni en desacuerdo",
+                               "De acuerdo",
+                               "Totalmente de acuerdo"), ordered = T)
+
+# Plot it (SAVE IT!!!!)
+gg <- fqTable %>% ggplot(aes(x = Categoria, y = Porcentaje*100)) +
+  geom_bar(stat = "identity") +
+  xlab("") + ylab("Porcentaje (%)") +
+  coord_flip() +
+  facet_wrap(~ Variable, scales = "free") +
+  theme_bw() +
+  theme(strip.text = element_text(size = 12, face = "bold")) +
+  theme(axis.title.x = element_text(size = 13, face = 'bold'),
+        axis.title.y = element_text(size = 13, face = 'bold'),
+        axis.text = element_text(size = 12))
+ggsave("../_results/_descriptive_analysis/frecuencias_aprendizaje.png", plot = gg, width = 22, height = 10, units = "in"); rm(fqTable, gg)
+
 
 options(warn=-1)
 p.chisq = matrix(0, nrow=ncol(aprendizaje), ncol=ncol(aprendizaje), byrow=T)
@@ -26,7 +90,7 @@ heatmap.2(p.chisq,
           main="Independence test",
           key.title="Chi-square test",
           key.xlab="p-value",
-          Rowv=NULL,
+          Rowv=TRUE,
           Colv=NULL,
           col=color_scale,
           linecol=NULL,
